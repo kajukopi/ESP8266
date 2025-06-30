271f65ed9121fd0f4e2b7c49c74a84ac49c2b854
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-06-30 16:19:39 +0700

    Update ESP8266.ino

--- ESP8266.ino
+++ ESP8266.ino
@@ -1,37 +1,67 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoOTA.h>
+#include <ESP8266HTTPUpdateServer.h>
 
-// Replace these with your WiFi credentials
 const char* ssid = "karimroy";
 const char* password = "09871234";
 
-// Create web server on port 80
 ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+const int ledPin = 2; // Onboard LED
 
 void handleRoot() {
-  server.send(200, "text/html", "<h1>Hello from NodeMCU</h1><p>OTA Ready</p>");
+  String html = R"rawliteral(
+    <h1>NodeMCU LED Control</h1>
+    <p>LED is <b>%STATE%</b></p>
+    <a href="/led/on"><button>Turn ON</button></a>
+    <a href="/led/off"><button>Turn OFF</button></a>
+    <p><a href="/update">OTA Firmware Upload</a></p>
+  )rawliteral";
+
+  html.replace("%STATE%", digitalRead(ledPin) == LOW ? "ON" : "OFF");
+  server.send(200, "text/html", html);
+}
+
+void handleLedOn() {
+  digitalWrite(ledPin, LOW); // LED ON (active LOW)
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void handleLedOff() {
+  digitalWrite(ledPin, HIGH); // LED OFF
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // Start with LED OFF
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println();
+  Serial.println("WiFi connected at: ");
   Serial.println(WiFi.localIP());
 
-  // Set up web server routes
+  // Web server routes
   server.on("/", handleRoot);
+  server.on("/led/on", handleLedOn);
+  server.on("/led/off", handleLedOff);
+
+  // OTA firmware page
+  httpUpdater.setup(&server); // /update
+
   server.begin();
   Serial.println("HTTP server started");
 
-  // OTA Setup
   ArduinoOTA.setHostname("NodeMCU-OTA");
   ArduinoOTA.begin();
   Serial.println("OTA Ready");
@@ -41,5 +71,3 @@ void loop() {
   server.handleClient();
   ArduinoOTA.handle();
 }
-
-// Edit File 3
